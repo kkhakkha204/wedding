26e9aee859b3d49066df6a9a37f5da6f61bbe8f1
Mohit Virli <virlimohit@gmail.com>
2025-05-27 15:54:17 +0530

    Footer hover animations

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -13,21 +13,62 @@ const FooterLinkItem = ({ link }: { link: FooterLink }) => {
   const onPointerOver = () => setHovered(true);
   const onPointerOut = () => setHovered(false);
   const onClick = () => window.open(link.url, '_blank');
+  const onPointerMove = (e: MouseEvent) => {
+    if (isMobile) return;
+    const hoverDiv = document.getElementById(`footer-link-${link.name}`);
+    gsap.to(hoverDiv, {
+      top: `${e.clientY + 14}px`,
+      left: `${e.clientX}px`,
+      duration: 0.6,
+    });
+  };
+
   const fontProps = {
     font: "./Vercetti-Regular.woff",
     fontSize: 0.2,
     color: 'white',
     onPointerOver,
+    onPointerMove,
     onPointerOut,
     onClick,
   };
 
   useEffect(() => {
+    if (!document.getElementById(`footer-link-${link.name}`)) {
+      const hoverDiv = document.createElement('div');
+      hoverDiv.id = `footer-link-${link.name}`;
+      hoverDiv.textContent = link.hoverText ?? link.name.toUpperCase();
+      hoverDiv.style.position = 'fixed';
+      hoverDiv.style.zIndex = '2';
+      hoverDiv.style.bottom = '0';
+      hoverDiv.style.opacity = '0';
+      hoverDiv.style.left = window.innerWidth / 2 + 'px';
+      hoverDiv.style.fontSize = '0.8rem';
+      hoverDiv.style.pointerEvents = 'none';
+      document.body.appendChild(hoverDiv);
+    }
+  }, [])
+
+  useEffect(() => {
     if (isMobile) return
+
+    const hoverDiv = document.getElementById(`footer-link-${link.name}`);
+
+    if (hovered) {
+      gsap.fromTo(hoverDiv, { opacity: 0 }, { opacity: 0.5, delay: 0.2 });
+    } else {
+      gsap.to(hoverDiv, { opacity: 0 });
+    }
+
     gsap.to(textRef.current, {
       letterSpacing: hovered ? 0.3 : 0,
       duration: 0.3,
     });
+
+    return () => {
+      gsap.killTweensOf(hoverDiv);
+      gsap.killTweensOf(textRef.current);
+    }
   }, [hovered]);
 
   useCursor(hovered);
